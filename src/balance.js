f5e5e5393255f6eb5ff3a69a561d86f58d548b0b
havettaa <aurel.havetta@outlook.com>
2022-10-25 21:28:26 +0200

    fetchTicker

--- src/balance.js
+++ src/balance.js
@@ -3,15 +3,25 @@ import { log } from "./utils/logger.js";
 import { getExchange } from "./store.js";
 
 export async function balance() {
-  const res = await getExchange().fetch_balance();
-  log(res.BTC);
-  log(res.ETH);
-  log(res.USDT);
+  const res = await getExchange().fetchBalance();
+  log(res);
+  log(res.free[`BTC`]);
+  log(res.free[`ETH`]);
+  log(res.free[`USDT`]);
 }
 
+export async function fetchTicker() {
+  const res = await getExchange().fetchTicker(process.env.symbol);
+  return res;
+}
+
+export async function openOrders() {
+  const res = await getExchange().fetchOpenOrders(process.env.symbol);
+  log(res);
+}
 
 export async function orderBook() {
-  const res = await getExchange().fetch_order_book(process.env.symbol);
+  const res = await getExchange().fetchOrderBook(process.env.symbol);
   log(res);
 }
 
--- src/mainRun.js
+++ src/mainRun.js
@@ -1,4 +1,4 @@
-import { balance, orderBook } from "./balance.js";
+import { balance, fetchTicker, openOrders, orderBook } from "./balance.js";
 import { btcPrice } from "./coingecko.js";
 import { exchanges, fetchMarkets, kukoinZeroFees } from "./exchange.js";
 import { log } from "./utils/logger.js";
@@ -7,15 +7,11 @@ const mainRun = async () => {
 
   log(`Trading ${process.env.symbol} on ${process.env.exchange}`);
 
-  balance();
-
-  // fetchMarkets();
-
-  // kukoinZeroFees();
-
-  // setInterval(async () => {
-  //   log(await btcPrice(`bitcoin`));
-  // }, 5000);
+  setInterval(async () => {
+    //log(await btcPrice(`bitcoin`));
+    const t = await fetchTicker();
+    log(t.last);
+  }, 1000);
 }
 
 export { mainRun };
