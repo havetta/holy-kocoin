c071a0700020146a923c3200f605e79740f0f60b
aurel <to@be.defined>
2024-02-15 11:01:40 +0100

    Update main.js

--- src/main.js
+++ src/main.js
@@ -3,7 +3,10 @@ import { createRouter, createWebHashHistory } from 'vue-router'
 import './css/common.css'
 import App from './App.vue'
 import AppRoot from './AppRoot.vue'
-import components from "../public/datatypes/components.js"
+// import components from "../public/datatypes/components.js"
+let microsite = (new URLSearchParams(window.location.search)).get('microsite');
+// const microsite = router.query?.microsite;
+const components = (await import(`/datatypes/${microsite}.js?t=${Date.now()}`)).default;
 
 const routes = [{ path: '/', component: AppRoot }];
 components.forEach(c => routes.push({path: `/${c.name}`, component: c.instance}) );
