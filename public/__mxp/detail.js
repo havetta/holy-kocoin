473e5563ae1cb90a14589568a7e65ea6e882f83a
havettaa <45433460+havettaa@users.noreply.github.com>
2024-02-20 23:41:43 +0100

    form

--- public/__mxp/detail.js
+++ public/__mxp/detail.js
@@ -23,11 +23,34 @@ export default {
   },
   template: `
   <div>
-    <form>
-      <label for="acronym">Acronym</label>
+    <form class="shadow-md rounded px-8 pt-6 pb-8 mb-4">
+      <label for="acronym">Acronym: <sup class="p-1 border rounded-full">Will be in url and filename, don't use special chars.</sup></label>
       <input :value="state.list.find(i => i?.id === state.selectedId)?.acronym" placeholder="Acronym" type="text" />
       <textarea :value="state.list.find(i => i?.id === state.selectedId)?.texthtml" rows="4" class="block p-2.5 w-full text-sm text-gray-900 bg-gray-50 rounded-lg border border-gray-300 focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"></textarea>
       <textarea :value="state.list.find(i => i?.id === state.selectedId)?.textscript" rows="4" class="block p-2.5 w-full text-sm text-gray-900 bg-gray-50 rounded-lg border border-gray-300 focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"></textarea>
+
+      <div class="mb-4">
+        <label class="block text-gray-700 text-sm font-bold mb-2" for="username">
+          Username
+        </label>
+        <input class="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline" id="username" type="text" placeholder="Username">
+      </div>
+      <div class="mb-6">
+        <label class="block text-gray-700 text-sm font-bold mb-2" for="password">
+          Password
+        </label>
+        <input class="shadow appearance-none border border-red-500 rounded w-full py-2 px-3 text-gray-700 mb-3 leading-tight focus:outline-none focus:shadow-outline" id="password" type="password" placeholder="******************">
+        <p class="text-red-500 text-xs italic">Please choose a password.</p>
+      </div>
+      <div class="flex items-center justify-between">
+        <button class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline" type="button">
+          Sign In
+        </button>
+        <a class="inline-block align-baseline font-bold text-sm text-blue-500 hover:text-blue-800" href="#">
+          Forgot Password?
+        </a>
+      </div>
+
 
       <div class="flex flex-col gap-6 w-96">
         <div class="relative w-full min-w-[200px]">
