6df1087e37bf1cb25a426466afc6a2d4343af0c4
havettaa <45433460+havettaa@users.noreply.github.com>
2024-02-24 11:59:11 +0100

    favico

--- public/_js/common/_server.js
+++ public/_js/common/_server.js
@@ -87,7 +87,7 @@ server.get('/', async (req, res) => {
       <head>
         <meta charset="UTF-8" />
         <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-        <link rel="icon" type="image/svg+xml" href="/vite.svg" />
+        <link rel="icon" type="image/svg+xml" href="/favicon.svg" />
         <script type="importmap">
           { "imports": {
             "@vue/devtools-api": "./_npm/devtools-api/index.js",
