3e8e37f9e0af350a379dcbe78c0ae97c2024f909
havettaa <45433460+havettaa@users.noreply.github.com>
2024-12-02 08:45:44 +0100

    mexc no order

--- mexc.js
+++ mexc.js
@@ -79,19 +79,16 @@ const main = async () => {
 
   // console.log(await client.cancelAllOrders({}));
 
-  const avg = await client.avgPrice({})
-  console.log(`avgPrice = ${+avg.price}`);
+  const avg = await client.avgPrice({});    console.log(`avgPrice = ${+avg.price}`);
 
   // console.log(await client.order({ quantity: 10, price: +avg.price - 0.12 }));
   // console.log(await client.order({ quantity: 10, price: +avg.price + 0.22, side: 'SELL' }));
 
-  // console.log(await client.order({ quantity: 10, price: 13.46, side: 'SELL'}));
-  // console.log(await client.order({ quantity: 10, price: 12.84, }));
+  // console.log(await client.order({ quantity: 10, price: 13.45, side: 'SELL'}));
+  // console.log(await client.order({ quantity: 10, price: 12.11, }));
 
-  const bal = await client.account({})
-  console.log(bal?.balances);
-  const openOrd = await client.openOrders({});
-  console.table(openOrd.map(o => ({ side: o.side.substring(0, 3), price: o.price, qty: o.origQty, quote: o.origQuoteOrderQty, sym: o.symbol, type: o.type })));
+  const bal = await client.account({});    console.log(bal?.balances);
+  const openOrd = await client.openOrders({});    console.table(openOrd.map(o => ({ side: o.side.substring(0, 3), price: o.price, qty: o.origQty, quote: o.origQuoteOrderQty, sym: o.symbol, type: o.type })));
 
   // const ret = await client.futuresOrder({ vol: 2, price: avg.price, });
 };
